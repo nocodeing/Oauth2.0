28d8c95e04d3d848fec996334dc5b22ffd51e640
宋卫星 <swx13783367091@126.com>
2016-03-20 19:14:53 +0800

    创建项目

---
+++ authService/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace authService.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ authService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="authService.WebApiApplication" Language="C#" %>
